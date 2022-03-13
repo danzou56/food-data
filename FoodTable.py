04d73a3f5de3ad6c366dd1ae63d748898a1e7a50
Dan Zou <danzou12@umd.edu>
2022-03-13 18:53:57 -0400

    Refactor table init

--- FoodTable.py
+++ FoodTable.py
@@ -17,39 +17,52 @@ class FoodTable:
     def __init__(self, name, data_type):
         self.name = name
         self.data_type = data_type
+        self._df = None
 
         if FoodTable._data_con is None:
             FoodTable._data_con = sqlite3.connect(DB_PATH)
 
-        # if FoodTable.units_df is None:
-        #     FoodTable.units_df = pd.read_sql_table(table_name="conversions", con=FoodTable._data_con)
-
         if FoodTable.base_df is None:
-            units_df = pd.read_csv(os.path.join(DATA_PATH, "measure_unit.csv"), dtype="string").drop(columns="name")
-            # units_df.loc[units_df["name"] == "Tablespoons", "name"] = "tablespoon"
-            conversions_df = pd.read_sql_query(sql="SELECT * FROM conversions", con=FoodTable._data_con)
-            units_df = units_df.merge(
-                conversions_df,
-                on="id",
-                how="outer"
-            )
+            FoodTable._init_base_df()
 
-            food_df = pd.read_csv(os.path.join(DATA_PATH, "food.csv"), dtype="string").drop(
-                columns=["food_category_id", "publication_date"])
-            portions_df = pd.read_csv(os.path.join(DATA_PATH, "food_portion.csv"), dtype="string").drop(
-                columns=["min_year_acquired", "data_points", "footnote"])
+        self._dirty_df: pd.DataFrame = FoodTable.base_df[FoodTable.base_df["data_type"] == data_type]
 
-            FoodTable.base_df = food_df.merge(portions_df, on="fdc_id") \
-                .merge(units_df.rename(columns={"id": "measure_unit_id"}), on="measure_unit_id") \
-                .drop(columns=["measure_unit_id"])
+    @staticmethod
+    def _init_base_df():
+        units_df = pd.read_csv(os.path.join(DATA_PATH, "measure_unit.csv"), dtype="string")
+        # units_df.loc[units_df["name"] == "Tablespoons", "name"] = "tablespoon"
+        conversions_df = pd.read_sql_query(sql="SELECT * FROM conversions", con=FoodTable._data_con)
+        units_df = units_df.merge(
+            conversions_df,
+            on="id",
+            how="outer"
+        )
+        FoodTable.units_df = units_df
+        units_df = units_df.drop(columns="name")
 
-        self.df: pd.DataFrame = FoodTable.base_df[FoodTable.base_df["data_type"] == data_type]
-        self._clean()
-        self.df = self.df.dropna(subset=["equiv"])
-        self.df["gram_weight"] = self.df["gram_weight"].astype(np.float64)
-        self.df["amount"] = self.df["amount"].astype(np.float64)
-        self.df["g_per_ml"] = self.df["gram_weight"] / self.df["amount"] * self.df["equiv"]
-        self.df = self.df.drop(columns=["amount", "gram_weight", "unit_name", "equiv"])
+        food_df = pd.read_csv(os.path.join(DATA_PATH, "food.csv"), dtype="string").drop(
+            columns=["food_category_id", "publication_date"])
+        portions_df = pd.read_csv(os.path.join(DATA_PATH, "food_portion.csv"), dtype="string").drop(
+            columns=["min_year_acquired", "data_points", "footnote"])
+
+        FoodTable.base_df = food_df.merge(portions_df, on="fdc_id") \
+            .merge(units_df.rename(columns={"id": "measure_unit_id"}), on="measure_unit_id") \
+            .drop(columns=["measure_unit_id"])
 
     def _clean(self):
         pass
+
+    def _make(self):
+        self._df = self._dirty_df
+        self._clean()
+        self._df = self._df.dropna(subset=["equiv"])
+        self._df["gram_weight"] = self._df["gram_weight"].astype(np.float64)
+        self._df["amount"] = self._df["amount"].astype(np.float64)
+        self._df["g_per_ml"] = self._df["gram_weight"] / self._df["amount"] * self._df["equiv"]
+        self._df = self._df.drop(columns=["amount", "gram_weight", "equiv"])
+
+    def get_df(self):
+        if self._df is not None:
+            return self._df
+
+        self._make()
--- FoundationTable.py
+++ FoundationTable.py
@@ -11,13 +11,13 @@ class FoundationTable(FoodTable):
     def _clean(self):
         # this is the only instance where portion_description is non-nan so modify the description to
         # drop the portion_description column
-        self.df.loc[self.df["id"] == "119620", "description"] = "Cheese, cheddar, shredded"
-        self.df.loc[self.df["id"] == "119620", "portion_description"] = np.nan
+        self._df.loc[self._df["id"] == "119620", "description"] = "Cheese, cheddar, shredded"
+        self._df.loc[self._df["id"] == "119620", "portion_description"] = np.nan
         # make sure there's no more non-nans, just in case data changes in future
-        assert len(self.df.dropna(subset="portion_description")) == 0
-        self.df = self.df.drop(columns="portion_description")
-        self.df["description"] = self.df["description"].apply(lambda s: s.strip())
-        self.df = self.df.apply(get_joinner("description", "modifier"), axis=1).drop(columns="modifier")
+        assert len(self._df.dropna(subset="portion_description")) == 0
+        self._df = self._df.drop(columns="portion_description")
+        self._df["description"] = self._df["description"].apply(lambda s: s.strip())
+        self._df = self._df.apply(get_joinner("description", "modifier"), axis=1).drop(columns="modifier")
 
 
 if __name__ == '__main__':

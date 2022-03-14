bc1b038b8d1a25bccd68a55a7ea70e936c820922
Dan Zou <danzou12@umd.edu>
2022-03-14 13:32:20 -0400

    Refactor make_db and csvs

--- make_db.py
+++ make_db.py
@@ -5,8 +5,8 @@ import sqlite3
 from FoodTables.FoundationTable import FoundationTable
 
 if __name__ == '__main__':
-    u_df = pd.read_csv("units.csv", dtype={"id": "string", "name": "string", "equiv": np.float64})
-    u_df.drop(columns="name").to_sql(
+    u_df = pd.read_csv("conversions.csv", dtype={"id": "string", "name": "string", "equiv": np.float64})
+    u_df.to_sql(
         name="unit_conversions",
         con=sqlite3.connect("data.sqlite"),
         if_exists="replace",
